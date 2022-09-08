ec90877cde61fcc61cad8b4d3c01c14e092da9ad
unknown <sulthanftr@gmail.com>
2022-09-08 09:14:08 +0700

    fix Homepage read more button's link

--- src/components/Home.js
+++ src/components/Home.js
@@ -69,7 +69,7 @@ const Home = () => {
                     automotive by creating groundbreaking innovation, by create an inclusive environment
                     and making positive impact in the world of motorsport.
                 </p>
-                <a className="btn btn-primary btn-lg" href="/about" onclick="return redirectMe(this);">Read More</a>
+                <Link to="/about" className="btn btn-primary btn-lg">Read More</Link>
             </div>
             <div id="about-us" className="m-3 p-3 overflow-hidden mx-auto"></div>
         </div>
