38af42d493814a50490d2c54508f584804708d4a
sulthanftr <sulthanftr@gmail.com>
2023-05-13 22:19:39 +0700

    Clear netlify.toml

---
+++ out/_next/static/Nh5JX0pqozAJOutTi6M3E/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fnews","\u002Fnews\u002F[id]","\u002Fsponsors","\u002Fteam","\u002Fteam\u002Fev","\u002Fteam\u002Fgokart","\u002Fteam\u002Fmarketing"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
